3bb9d371d55b696c2808c18ab050b49c43625aa6
Alex Troupe <adtroupe@uw.edu>
2015-11-02 13:49:39 -0800

    lab shit

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,15 @@
 // Create application
 
+var myApp = angular.module('myApp', [])
 
 // Bind controller madCtrl
 
+myApp.controller('madCtrl', function($scope) {
+    $scope.name = 'Alex'
+    $scope.age = '21'
+    $scope.location = 'Xbox Pitch'
+    $scope.food = 'syrup'
+})
 
 	// Set initial values for name, age, location, and food
 
